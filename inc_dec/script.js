5edd85bb9edc4a2dcb4b1d1965594b4883f230c2
syed-imadulla <imadulla.s45@gmail.com>
2026-04-19 21:06:41 +0530

    added audio

--- inc_dec/script.js
+++ inc_dec/script.js
@@ -3,30 +3,46 @@ let res = document.getElementById("reset");
 let inc = document.getElementById("increment");
 let digit = document.querySelector(".digit");
 let gif = document.getElementById("specialGif");
+let sound = document.getElementById("bgSound");
 
-function updateUI() {
+let played = false;
+
+
+function updateUI(){
     let num = parseInt(digit.textContent);
 
-    if (num === 67) {
+    if(num === 67){
         digit.style.color = "yellow";
         gif.classList.add("show");
-    } else {
-        digit.style.color = "#e3e3e3";
+
+        if(!played){
+            sound.currentTime = 0;
+            sound.play();
+            played = true;
+        }
+
+    }else{
+        digit.style.color = "#e8e6e3";
         gif.classList.remove("show");
+
+        sound.pause();          
+        sound.currentTime = 0;  
+
+        played = false;
     }
 }
 
-dec.addEventListener("click", () => {
+dec.addEventListener("click",()=>{
     digit.textContent = parseInt(digit.textContent) - 1;
     updateUI();
 });
 
-res.addEventListener("click", () => {
-    digit.textContent = "0";
+res.addEventListener("click",()=>{
+    digit.textContent = 0;
     updateUI();
 });
 
-inc.addEventListener("click", () => {
+inc.addEventListener("click",()=>{
     digit.textContent = parseInt(digit.textContent) + 1;
     updateUI();
 });
